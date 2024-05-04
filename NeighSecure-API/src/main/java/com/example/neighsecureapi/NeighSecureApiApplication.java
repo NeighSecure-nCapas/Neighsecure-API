7d51c58e152c7a6335b13163d8f5713e6cde3d73
cheesypops <00030821@uca.edu.sv>
2024-05-04 17:14:53 -0600

    Add: packages and utils: errorsTools

---
+++ NeighSecure-API/src/main/java/com/example/neighsecureapi/NeighSecureApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.neighsecureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighSecureApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeighSecureApiApplication.class, args);
+    }
+
+}
---
+++ NeighSecure-API/src/test/java/com/example/neighsecureapi/NeighSecureApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.neighsecureapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeighSecureApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
