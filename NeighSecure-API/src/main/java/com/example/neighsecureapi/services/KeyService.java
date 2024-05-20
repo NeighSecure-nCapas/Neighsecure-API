ee37b77b3da30af0863035a623d482cfa67acb8d
cheesypops <00030821@uca.edu.sv>
2024-05-20 11:57:17 -0600

    Add: keyServiceImpl, KeyUpdateDTO

--- NeighSecure-API/src/main/java/com/example/neighsecureapi/services/KeyService.java
+++ NeighSecure-API/src/main/java/com/example/neighsecureapi/services/KeyService.java
@@ -1,18 +1,20 @@
 package com.example.neighsecureapi.services;
 
+import com.example.neighsecureapi.domain.dtos.KeyUpdateDTO;
 import com.example.neighsecureapi.domain.entities.Key;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface KeyService {
     // CRUD IMPLEMENTATION FOR KEY ENTITY
     // TODO: aplicar dtos
-    public void saveKey(String keyName, String keyAddress);
+    public void saveKey();
     public void deleteKey(String keyId);
 
     // hay q validar la vigencia del permiso
-    public void updateKey(String keyName, String keyAddress);
-    public Key getKey(String keyName);
+    public void updateKey(Key key, KeyUpdateDTO info);
+    public Key getKey(UUID keyId);
     public List<Key> getAllKeys();
 
     // END OF CRUD IMPLEMENTATION ---------------------------------------------------------------------
---
+++ NeighSecure-API/src/main/java/com/example/neighsecureapi/services/serviceImpl/KeyServiceImplementation.java
@@ -0,0 +1,53 @@
+package com.example.neighsecureapi.services.serviceImpl;
+
+import com.example.neighsecureapi.domain.dtos.KeyUpdateDTO;
+import com.example.neighsecureapi.domain.entities.Key;
+import com.example.neighsecureapi.repositories.KeyRepository;
+import com.example.neighsecureapi.services.KeyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class KeyServiceImplementation implements KeyService {
+
+    private final KeyRepository keyRepository;
+
+    public KeyServiceImplementation(KeyRepository keyRepository) {
+        this.keyRepository = keyRepository;
+    }
+
+    @Override
+    public void saveKey() {
+        // esta vacio ya que es la primera generacion de la llave, se inicia con valores null
+        // quiza no sea ni necesaria la funcion, directamente se crea una llave en el controller
+
+    }
+
+    @Override
+    public void deleteKey(String keyId) {
+
+    }
+
+    @Override
+    public void updateKey(Key key, KeyUpdateDTO info) {
+        key.setGenerationDate(info.getGenerationDate());
+        key.setGenerationTime(info.getGenerationTime());
+        key.setGenerationDay(info.getGenerationDay());
+
+        keyRepository.save(key);
+    }
+
+    @Override
+    public Key getKey(UUID keyId) {
+        return keyRepository.findById(keyId).orElse(null);
+    }
+
+    @Override
+    public List<Key> getAllKeys() {
+        return keyRepository.findAll();
+        // innecesario
+    }
+}
