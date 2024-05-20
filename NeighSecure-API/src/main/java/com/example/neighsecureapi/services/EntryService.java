9c8daab4f2f6d1222e65e7136936ec0226d1812d
cheesypops <00030821@uca.edu.sv>
2024-05-20 12:15:09 -0600

    Add: entryServiceImpl, EntryRegisterDTO

--- NeighSecure-API/src/main/java/com/example/neighsecureapi/services/EntryService.java
+++ NeighSecure-API/src/main/java/com/example/neighsecureapi/services/EntryService.java
@@ -1,29 +1,32 @@
 package com.example.neighsecureapi.services;
 
+import com.example.neighsecureapi.domain.dtos.EntryRegisterDTO;
 import com.example.neighsecureapi.domain.entities.Entry;
+import com.example.neighsecureapi.domain.entities.Permission;
+import com.example.neighsecureapi.domain.entities.Terminal;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface EntryService {
 
     // CRUD IMPLEMENTATION FOR ENTRY ENTITY
-    // TODO: aplicar dtos
-    public void saveEntry(String entryName, String entryAddress);
-    public void deleteEntry(String entryId);
-    public void updateEntry(String entryName, String entryAddress);
-    public Entry getEntry(String entryName);
+    public void saveEntry(EntryRegisterDTO info, Terminal terminal, Permission permission);
+    public void deleteEntry(UUID entryId);
+    // public void updateEntry(String entryName, String entryAddress);
+    public Entry getEntry(UUID entryId);
 
     public List<Entry> getAllEntries();
 
     // END OF CRUD IMPLEMENTATION ---------------------------------------------------------------------
 
     // ADDITIONAL METHODS
-    public void addKeyToEntry(String entryId, String key);
+    // public void addKeyToEntry(String entryId, String key);
 
     // recibir el numero de visitas en un dia
-    public Integer getEntriesByDate(String date);
+    // public Integer getEntriesByDate(String date);
 
     // numero de visitantes por hora en un dia
-    public Integer getEntriesByHour(String date, String hour);
+    // public Integer getEntriesByHour(String date, String hour);
 
 }
