87486ed5b564784d9f0aed839d53ec74438e5b97
cheesypops <00030821@uca.edu.sv>
2024-06-06 13:15:51 -0600

    First commit

---
+++ NeighSecure-API/src/main/java/com/example/neighsecureapi/controllers/HomeBossController.java
@@ -0,0 +1,19 @@
+package com.example.neighsecureapi.controllers;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/homeboss")
+@CrossOrigin
+@Slf4j
+public class HomeBossController {
+
+
+
+
+
+}
