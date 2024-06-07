1cc97fce13955d8d521013b83f76deff477fa6e9
cheesypops <00030821@uca.edu.sv>
2024-06-07 14:17:09 -0600

    first commit

---
+++ NeighSecure-API/src/main/java/com/example/neighsecureapi/controllers/VisitController.java
@@ -0,0 +1,13 @@
+package com.example.neighsecureapi.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/neighSecure/visit")
+@Slf4j
+public class VisitController {
+
+
+}
